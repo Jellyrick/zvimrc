df725d6f80828cca1e96cc1d4c77cfe6bee13fa2
Jiguo Zhang <75199165@qq.com>
2017-02-16 14:52:41 +0800

    support master deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,25 @@
-ln ~/.vim/.vimrc -fs ~/
-ln ~/.vim/.zshrc -fs ~/
-ln ~/.vim/.tmux.conf -fs ~/
+MACHINE=NODE
+while [ "$1" != "" ]
+do
+    case "$1" in
+        "--machine")
+            shift
+            MACHINE="$1"
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+if [ $MACHINE == "NODE" ]; then
+	ln -fs ~/.vim/.tmux.conf ~/
+	ln -fs ~/.vim/.zshrc ~/
+elif [ $MACHINE == "MASTER" ]; then
+	ln -fs ~/.vim/.tmux.conf.base ~/.tmux.conf
+	ln -fs ~/.vim/.zshrc.macos ~/.zshrc
+else
+	echo "UNKNOWN MACHINE TYPE"
+fi
+ln -fs ~/.vim/.vimrc ~/
+ln -fs ~/.vim/.gitconfig ~/
