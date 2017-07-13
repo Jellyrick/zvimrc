23ed77266c0e9a2bd7bb66a1cc0704a3be6ca404
Jiguo Zhang <75199165@qq.com>
2017-07-13 15:56:14 +0800

    add remote deploy

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -1,6 +1,12 @@
-pssh -H '172.25.52.17 172.25.52.18' "cd /home/zhangjiguo; rm -fr ~/.vim"
-prsync -H '172.25.52.88 172.25.52.69' /usr/local/zsh-master /home/zhangjiguo/
-prsync -H '172.25.52.88 172.25.52.69' /usr/local/tmux-master /home/zhangjiguo/
-pssh -H '172.25.52.88 172.25.52.69' "sudo mv /home/zhangjiguo/tmux-master /usr/local"
-pssh -H '172.25.52.88 172.25.52.69' "sudo mv /home/zhangjiguo/zsh-master /usr/local"
-pssh -H '172.25.52.88 172.25.52.69' "sudo chown -R root:root /usr/local/tmux-master /usr/local/zsh-master"
+#!/bin/bash
+MACHINES='172.25.52.93'
+pssh -H $MACHINES "cd /home/zhangjiguo; rm -fr ~/.vim"
+prsync -avzrH $MACHINES /home/zhangjiguo/.vim /home/zhangjiguo/
+prsync -avzr -H $MACHINES /usr/local/zsh-master /home/zhangjiguo/
+prsync -avzr -H $MACHINES /usr/local/tmux-master /home/zhangjiguo/
+prsync -avzr -H $MACHINES /usr/local/vim-8.0.0599 /home/zhangjiguo/
+pssh -t0 -P -x '-A -tt' -H $MACHINES "sudo mv /home/zhangjiguo/tmux-master /usr/local"
+pssh -t0 -P -x '-A -tt' -H $MACHINES "sudo mv /home/zhangjiguo/zsh-master /usr/local"
+pssh -t0 -P -x '-A -tt' -H $MACHINES "sudo mv /home/zhangjiguo/vim-8.0.0599 /usr/local"
+pssh -t0 -P -x '-A -tt' -H $MACHINES "sudo chown -R root:root /usr/local/tmux-master /usr/local/zsh-master /usr/local/vim-8.0.0599"
+pssh -t0 -P -x '-A -tt' -H $MACHINES sudo chsh -s /usr/local/zsh-master/bin/zsh zhangjiguo
