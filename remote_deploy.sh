f383dee313e547327acba270892934f1b9ffdced
Jiguo Zhang <75199165@qq.com>
2017-08-25 17:11:34 +0800

    remote deploy support multi machines

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -5,12 +5,12 @@ if [ $# -lt 1 ]; then
 fi
 MACHINES=$1
 pssh -H $MACHINES "cd /home/zhangjiguo; rm -fr ~/.vim"
-prsync -avzrH $MACHINES /home/zhangjiguo/.vim /home/zhangjiguo/
-prsync -avzr -H $MACHINES /usr/local/zsh-master /home/zhangjiguo/
-prsync -avzr -H $MACHINES /usr/local/tmux-master /home/zhangjiguo/
-prsync -avzr -H $MACHINES /usr/local/vim-8.0.0599 /home/zhangjiguo/
-pssh -t0 -P -x '-A -tt' -H $MACHINES "sudo mv /home/zhangjiguo/tmux-master /usr/local"
-pssh -t0 -P -x '-A -tt' -H $MACHINES "sudo mv /home/zhangjiguo/zsh-master /usr/local"
-pssh -t0 -P -x '-A -tt' -H $MACHINES "sudo mv /home/zhangjiguo/vim-8.0.0599 /usr/local"
-pssh -t0 -P -x '-A -tt' -H $MACHINES "sudo chown -R root:root /usr/local/tmux-master /usr/local/zsh-master /usr/local/vim-8.0.0599"
-pssh -t0 -P -x '-A -tt' -H $MACHINES sudo chsh -s /usr/local/zsh-master/bin/zsh zhangjiguo
+prsync -avzrH "$MACHINES" /home/zhangjiguo/.vim /home/zhangjiguo/
+prsync -avzrH "$MACHINES" /usr/local/zsh-master /home/zhangjiguo/
+prsync -avzrH "$MACHINES" /usr/local/tmux-master /home/zhangjiguo/
+prsync -avzrH "$MACHINES" /usr/local/vim-8.0.0599 /home/zhangjiguo/
+pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo mv /home/zhangjiguo/tmux-master /usr/local"
+pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo mv /home/zhangjiguo/zsh-master /usr/local"
+pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo mv /home/zhangjiguo/vim-8.0.0599 /usr/local"
+pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo chown -R root:root /usr/local/tmux-master /usr/local/zsh-master /usr/local/vim-8.0.0599"
+pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo chsh -s /usr/local/zsh-master/bin/zsh zhangjiguo"
