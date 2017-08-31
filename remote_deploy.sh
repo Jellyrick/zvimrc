b79c08335c9244f997ffa09db6d87164eda2d033
Jiguo Zhang <75199165@qq.com>
2017-08-31 18:22:50 +0800

    fix bug

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -9,8 +9,8 @@ prsync -avzrH "$MACHINES" /home/zhangjiguo/.vim /home/zhangjiguo/
 prsync -avzrH "$MACHINES" /usr/local/zsh-master /home/zhangjiguo/
 prsync -avzrH "$MACHINES" /usr/local/tmux-master /home/zhangjiguo/
 prsync -avzrH "$MACHINES" /usr/local/vim-8.0.0599 /home/zhangjiguo/
-pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo mv /home/zhangjiguo/tmux-master /usr/local"
-pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo mv /home/zhangjiguo/zsh-master /usr/local"
-pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo mv /home/zhangjiguo/vim-8.0.0599 /usr/local"
+pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo cp -fr /home/zhangjiguo/tmux-master /usr/local && rm -fr /home/zhangjiguo/tmux-master"
+pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo cp -fr /home/zhangjiguo/zsh-master /usr/local && rm -fr /home/zhangjiguo/zsh-master"
+pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo cp -fr /home/zhangjiguo/vim-8.0.0599 /usr/local && rm -fr /home/zhangjiguo/vim-8.0.0599"
 pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo chown -R root:root /usr/local/tmux-master /usr/local/zsh-master /usr/local/vim-8.0.0599"
 pssh -t0 -Px '-A -tt' -H "$MACHINES" "sudo chsh -s /usr/local/zsh-master/bin/zsh zhangjiguo"
